d276485575087e8fb7d3a71d5b8e08ee7317a8dc
Abdelkhalek ZELLAT <ha_zellat@esi.dz>
2020-01-02 05:16:40 +0100

    check for alerts in selenium

--- Selenium-Test/script.py
+++ Selenium-Test/script.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-web-security')
 
 url='http://127.0.0.1:8080'
-driver = webdriver.Chrome("./chromedriver")
+driver = webdriver.Chrome("./chromedriver" , chrome_options=options)
 driver.get(url)
 
 # Find add student button and click ! 
@@ -15,7 +21,7 @@ time.sleep(3)
 # Find all inputs and fill the form ! 
 
 driver.find_element_by_id("input-matricule").send_keys("17/068")
-driver.find_element_by_id("input-email").send_keys("hi_kara@esi.dz")
+driver.find_element_by_id("input-email").send_keys("hi_karaa@esi.dz")
 driver.find_element_by_id("input-nom").send_keys("Kara")
 driver.find_element_by_id("input-prenom").send_keys("Imene")
 driver.find_element_by_id("input-adresse").send_keys("Cité les oiseaux")
@@ -31,7 +37,20 @@ driver.find_element_by_id("input-groupe").send_keys("08")
 submitButton = driver.find_element_by_xpath('//*[@id="submit"]')
 submitButton.click()
 
+time.sleep(3)
+
 # Find list students button and click ! 
+
+try:
+    WebDriverWait(driver, 3).until(EC.alert_is_present(),
+                                   'Timed out waiting for PA creation ' +
+                                   'confirmation popup to appear.')
+
+    alert = driver.switch_to.alert
+    alert.accept()
+    print("alert accepted")
+except TimeoutException:
+    print("no alert")
 
 listLink = driver.find_element_by_xpath("//a[@href='/tables']")
 listLink.click()
